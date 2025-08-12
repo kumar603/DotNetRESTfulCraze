a448a6d3c5f37f6ecfac4ddd9e8d04fc65a937a8
Kiran Kumar <kumar603>
2025-08-12 19:37:35 +0530

    ASPNET RESTFUL APIS -  Consume APIS  - MVC Application

---
+++ Controllers/ProductsConsumeINMVCController.cs
@@ -0,0 +1,37 @@
+﻿using DotNetRESTfulCraze.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DotNetRESTfulCraze.Controllers
+{
+/*****************************************************************************************************
+Writer       : Kiran Kumar J
+Description  : Get to know about Creating Restful API with create new model Product and loading data from
+               Controllers and Consume the API from MVC Front UI application
+Created Date : 05 - August - 2025
+Created By   : Kiran Kumar
+Changed Date : 05 - August - 2025
+Changed By   : Kiran Kumar
+JIRA ID      : https://kiranjuvvanapudi.atlassian.net/browse/AMAECR-17
+*****************************************************************************************************/
+    public class ProductsConsumeINMVCController : ApiController
+    {
+        private static List<Product> products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Television", Price = 15000 },
+            new Product { Id = 2, Name = "Camera", Price = 50000 }
+        };
+
+        // GET api/products
+        [HttpGet]
+        [Route("api/productsConsumeNVC")]
+        public IEnumerable<Product> Get()
+        {
+            return products;
+        }
+    }
+}
---
+++ Controllers/ProductsMVCAPIController.cs
@@ -0,0 +1,52 @@
+﻿using DotNetRESTfulCraze.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DotNetRESTfulCraze.Controllers
+{
+    /*****************************************************************************************************
+    Writer       : Kiran Kumar J
+    Description  : Get to know about Creating Restful API with create new model Product and loading data from
+                   Controllers and Consume the API from MVC Front UI application
+    Created Date : 05 - August - 2025
+    Created By   : Kiran Kumar
+    Changed Date : 05 - August - 2025
+    Changed By   : Kiran Kumar
+    JIRA ID      : https://kiranjuvvanapudi.atlassian.net/browse/AMAECR-17
+    *****************************************************************************************************/
+    public class ProductsMVCAPIController : Controller
+    {
+        // Replace with your Web API port
+        private readonly string baseUrl = "http://localhost:65030//api/productsConsumeNVC";
+
+        // GET: Product
+        public async Task<ActionResult> Index()
+        {
+            var products = new List<Product>();
+
+            using (var client = new HttpClient())
+            {
+                // Send GET request to API
+                HttpResponseMessage response = await client.GetAsync(baseUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonData = await response.Content.ReadAsStringAsync();
+                    products = JsonConvert.DeserializeObject<List<Product>>(jsonData);
+                }
+                else
+                {
+                    ViewBag.Error = "Error fetching products from API";
+                }
+            }
+
+            return View(products);
+        }
+    }
+}
